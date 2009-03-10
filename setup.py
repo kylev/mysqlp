b1492c80fa95d843061b448cdec3a1b010f5d1d6
Kyle VanderBeek <kylev@kylev.com>
2009-03-10 14:22:50 -0700

    Just the basic stuff we need for a distro

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mysqlp",
+    version="0.1",
+    packages=find_packages(),
+    author = "Kyle VanderBeek",
+    author_email = "kylev@kylev.com",
+    description = "A pure-python MySQL client library.",
+)
