fae9f6f6bf35ff3e9b36ebb2b593ccaae07e93b5
Kyle VanderBeek <kylev@kylev.com>
2010-10-30 17:58:42 -0700

    Style fix

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     name="mysqlp",
     version="0.2",
     packages=find_packages(),
-    author = "Kyle VanderBeek",
-    author_email = "kylev@kylev.com",
-    description = "A pure-python MySQL client library.",
-    test_suite = 'nose.collector',
+    author="Kyle VanderBeek",
+    author_email="kylev@kylev.com",
+    description="A pure-python MySQL client library.",
+    test_suite='nose.collector',
 )
